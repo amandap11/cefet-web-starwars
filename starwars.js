0670c4dde785de17e91b6f07974863568f523587
Amanda <afp1103@gmail.com>
2017-04-25 09:19:39 -0300

    Alteração do 'ul'. Início da prática.

--- starwars.js
+++ starwars.js
@@ -3,3 +3,28 @@
 // para carregar:
 //  - A lista de filmes
 //  - A introdução de cada filme, quando ele for clicado
+$(document).ready(
+	function(){
+		$.ajax({
+		url: 'http://swapi.co/api/films/',
+		method: 'GET',
+		success: function(resposta){
+			atualizaLista(resposta);
+		}
+	});
+});
+
+function atualizaLista(resposta){
+
+	let listaFilmes = resposta.results;
+
+	for (var i = 0; i < listaFilmes.length; i++){
+
+		console.log(listaFilmes[i]);
+
+		$("ul").append("<li data-episode-url=" + listaFilmes[i].url + " + >Episódio " + 
+			listaFilmes[i].episode_id + "</li>");
+
+
+	}		
+}
